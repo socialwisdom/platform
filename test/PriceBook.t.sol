b1640e3bad86f655cc89fbcdebdd1d1e5957b724
0xsocialwisdom <cto@socialwisdom.io>
2025-11-30 15:34:27 +0400

    feat: impl default volume creation

--- test/PriceBook.t.sol
+++ test/PriceBook.t.sol
@@ -163,8 +163,18 @@ library OrderExt {
 }
 
 library PriceBookExt {
+    uint256 constant DEFAULT_VOLUME = 1_000;
+
+    function createDefaultBuyOrder(PriceBook priceBook, uint8 price) internal returns (uint256) {
+        return createBuyOrder(priceBook, price, DEFAULT_VOLUME);
+    }
+
     function createBuyOrder(PriceBook priceBook, uint8 price, uint256 volume) internal returns (uint256) {
         return priceBook.createOrder(address(0x42), price, true, volume);
+    }
+
+    function createDefaultSellOrder(PriceBook priceBook, uint8 price) internal returns (uint256) {
+        return createSellOrder(priceBook, price, DEFAULT_VOLUME);
     }
 
     function createSellOrder(PriceBook priceBook, uint8 price, uint256 volume) internal returns (uint256) {
