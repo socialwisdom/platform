3328cc7e9739d9363e728d6ed9db58dbc9af090e
0xsocialwisdom <cto@socialwisdom.io>
2025-12-10 14:50:30 +0400

    feat: poc test for conditional tokens and interface fixes

--- interfaces/IConditionalTokens.sol
+++ interfaces/IConditionalTokens.sol
@@ -1,25 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title IConditionalTokens
-/// @notice Interface for the Gnosis ConditionalTokensFramework: https://github.com/gnosis/conditional-tokens-contracts/contracts/ConditionalTokens.sol
-interface IConditionalTokens {
+/// @notice Interface for the Gnosis ConditionalTokensFramework: https://github.com/gnosis/conditional-tokens-contracts/blob/master/contracts/ConditionalTokens.sol
+interface IConditionalTokens is IERC1155 {
+    /// @dev Emitted upon the successful preparation of a condition.
+    /// @param conditionId The condition's ID. This ID may be derived from the other three parameters via ``keccak256(abi.encodePacked(oracle, questionId, outcomeSlotCount))``.
+    /// @param oracle The account assigned to report the result for the prepared condition.
+    /// @param questionId An identifier for the question to be answered by the oracle.
+    /// @param outcomeSlotCount The number of outcome slots which should be used for this condition. Must not exceed 256.
+    event ConditionPreparation(
+        bytes32 indexed conditionId,
+        address indexed oracle,
+        bytes32 indexed questionId,
+        uint outcomeSlotCount
+    );
+
+    event ConditionResolution(
+        bytes32 indexed conditionId,
+        address indexed oracle,
+        bytes32 indexed questionId,
+        uint outcomeSlotCount,
+        uint[] payoutNumerators
+    );
+
+    /// @dev Emitted when a position is successfully split.
+    event PositionSplit(
+        address indexed stakeholder,
+        IERC20 collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 indexed conditionId,
+        uint[] partition,
+        uint amount
+    );
+    /// @dev Emitted when positions are successfully merged.
+    event PositionsMerge(
+        address indexed stakeholder,
+        IERC20 collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 indexed conditionId,
+        uint[] partition,
+        uint amount
+    );
+    event PayoutRedemption(
+        address indexed redeemer,
+        IERC20 indexed collateralToken,
+        bytes32 indexed parentCollectionId,
+        bytes32 conditionId,
+        uint[] indexSets,
+        uint payout
+    );
+
+    /// Mapping key is an condition ID. Value represents numerators of the payout vector associated with the condition. This array is initialized with a length equal to the outcome slot count. E.g. Condition with 3 outcomes [A, B, C] and two of those correct [0.5, 0.5, 0]. In Ethereum there are no decimal values, so here, 0.5 is represented by fractions like 1/2 == 0.5. That's why we need numerator and denominator values. Payout numerators are also used as a check of initialization. If the numerators array is empty (has length zero), the condition was not created/prepared. See getOutcomeSlotCount.
     function payoutNumerators(bytes32 conditionId, uint256 index) external view returns (uint256);
 
+    /// Denominator is also used for checking if the condition has been resolved. If the denominator is non-zero, then the condition has been resolved.
     function payoutDenominator(bytes32 conditionId) external view returns (uint256);
 
     /// @dev This function prepares a condition by initializing a payout vector associated with the condition.
     /// @param oracle The account assigned to report the result for the prepared condition.
     /// @param questionId An identifier for the question to be answered by the oracle.
     /// @param outcomeSlotCount The number of outcome slots which should be used for this condition. Must not exceed 256.
-    function prepareCondition(address oracle, bytes32 questionId, uint256 outcomeSlotCount) external;
+    function prepareCondition(address oracle, bytes32 questionId, uint outcomeSlotCount) external;
 
     /// @dev Called by the oracle for reporting results of conditions. Will set the payout vector for the condition with the ID ``keccak256(abi.encodePacked(oracle, questionId, outcomeSlotCount))``, where oracle is the message sender, questionId is one of the parameters of this function, and outcomeSlotCount is the length of the payouts parameter, which contains the payoutNumerators for each outcome slot of the condition.
     /// @param questionId The question ID the oracle is answering for
     /// @param payouts The oracle's answer
-    function reportPayouts(bytes32 questionId, uint256[] calldata payouts) external;
+    function reportPayouts(bytes32 questionId, uint[] calldata payouts) external;
 
     /// @dev This function splits a position. If splitting from the collateral, this contract will attempt to transfer `amount` collateral from the message sender to itself. Otherwise, this contract will burn `amount` stake held by the message sender in the position being split worth of EIP 1155 tokens. Regardless, if successful, `amount` stake will be minted in the split target positions. If any of the transfers, mints, or burns fail, the transaction will revert. The transaction will also revert if the given partition is trivial, invalid, or refers to more slots than the condition is prepared with.
     /// @param collateralToken The address of the positions' backing collateral token.
@@ -31,8 +81,8 @@ interface IConditionalTokens {
         IERC20 collateralToken,
         bytes32 parentCollectionId,
         bytes32 conditionId,
-        uint256[] calldata partition,
-        uint256 amount
+        uint[] calldata partition,
+        uint amount
     ) external;
 
     /// @dev This function merges CTF tokens into the underlying collateral.
@@ -45,8 +95,8 @@ interface IConditionalTokens {
         IERC20 collateralToken,
         bytes32 parentCollectionId,
         bytes32 conditionId,
-        uint256[] calldata partition,
-        uint256 amount
+        uint[] calldata partition,
+        uint amount
     ) external;
 
     /// @dev This function redeems a CTF ERC1155 token for the underlying collateral
@@ -54,38 +104,27 @@ interface IConditionalTokens {
     /// @param parentCollectionId The ID of the outcome collections common to the position
     /// @param conditionId The ID of the condition to split on.
     /// @param indexSets Index sets of the outcome collection to combine with the parent outcome collection
-    function redeemPositions(
-        IERC20 collateralToken,
-        bytes32 parentCollectionId,
-        bytes32 conditionId,
-        uint256[] calldata indexSets
-    ) external;
+    function redeemPositions(IERC20 collateralToken, bytes32 parentCollectionId, bytes32 conditionId, uint[] calldata indexSets) external;
 
     /// @dev Gets the outcome slot count of a condition.
     /// @param conditionId ID of the condition.
     /// @return Number of outcome slots associated with a condition, or zero if condition has not been prepared yet.
-    function getOutcomeSlotCount(bytes32 conditionId) external view returns (uint256);
+    function getOutcomeSlotCount(bytes32 conditionId) external view returns (uint);
 
     /// @dev Constructs a condition ID from an oracle, a question ID, and the outcome slot count for the question.
     /// @param oracle The account assigned to report the result for the prepared condition.
     /// @param questionId An identifier for the question to be answered by the oracle.
     /// @param outcomeSlotCount The number of outcome slots which should be used for this condition. Must not exceed 256.
-    function getConditionId(address oracle, bytes32 questionId, uint256 outcomeSlotCount)
-        external
-        pure
-        returns (bytes32);
+    function getConditionId(address oracle, bytes32 questionId, uint outcomeSlotCount) external pure returns (bytes32);
 
     /// @dev Constructs an outcome collection ID from a parent collection and an outcome collection.
     /// @param parentCollectionId Collection ID of the parent outcome collection, or bytes32(0) if there's no parent.
     /// @param conditionId Condition ID of the outcome collection to combine with the parent outcome collection.
     /// @param indexSet Index set of the outcome collection to combine with the parent outcome collection.
-    function getCollectionId(bytes32 parentCollectionId, bytes32 conditionId, uint256 indexSet)
-        external
-        view
-        returns (bytes32);
+    function getCollectionId(bytes32 parentCollectionId, bytes32 conditionId, uint indexSet) external view returns (bytes32);
 
     /// @dev Constructs a position ID from a collateral token and an outcome collection. These IDs are used as the ERC-1155 ID for this contract.
     /// @param collateralToken Collateral token which backs the position.
     /// @param collectionId ID of the outcome collection associated with this position.
-    function getPositionId(IERC20 collateralToken, bytes32 collectionId) external pure returns (uint256);
+    function getPositionId(IERC20 collateralToken, bytes32 collectionId) external pure returns (uint);
 }
---
+++ test/ConditionalTokens.t.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {console2} from "forge-std/console2.sol";
+import {IConditionalTokens} from "../interfaces/IConditionalTokens.sol";
+import {Deploy} from "../libraries/Deploy.sol";
+import {Test} from "forge-std/Test.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract USDC is ERC20 {
+    uint256 public constant dec = 18;
+    uint256 public constant tokens = 10 ** dec;
+
+    uint256 public constant initSupply = 1_000_000 * tokens;
+
+    constructor() ERC20("TestToken", "TT") {
+        _mint(msg.sender, initSupply);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return uint8(dec);
+    }
+}
+
+contract ConditionalTokensTest is Test {
+    IConditionalTokens public conditionalTokens;
+    USDC public usdc;
+
+    address public constant oracle = address(0x09ac1e);
+
+    function setUp() public {
+        vm.startPrank(address(0x42));
+
+        console2.log("Oracle address:", oracle);
+
+        conditionalTokens = Deploy.conditionalTokens();
+
+        console2.log("CT address:", address(conditionalTokens));
+
+        usdc = new USDC();
+
+        console2.log("USDC address:", address(usdc));
+        console2.log("Balance of 0x42:", usdc.balanceOf(address(0x42)));
+    }
+
+    function test_conditionalTokens_poc() public {
+        bytes32 questionId = bytes32(uint256(1));
+        uint256 outcomeSlotCount = 2;
+        bytes32 conditionId = keccak256(abi.encodePacked(oracle, questionId, outcomeSlotCount));
+
+        vm.expectEmit();
+        emit IConditionalTokens.ConditionPreparation(conditionId, oracle, questionId, outcomeSlotCount);
+
+        conditionalTokens.prepareCondition(oracle, questionId, outcomeSlotCount);
+
+        bytes32 parentCollectionId = bytes32(0);
+
+        uint256[] memory partition = new uint256[](2);
+        partition[0] = 1; // 0b01
+        partition[1] = 2; // 0b10
+
+        usdc.approve(address(conditionalTokens), 1_000 * usdc.tokens());
+
+        conditionalTokens.splitPosition(usdc, parentCollectionId, conditionId, partition, 1_000 * usdc.tokens());
+
+        assertEq(usdc.balanceOf(address(0x42)), usdc.initSupply() - 1_000 * usdc.tokens());
+
+        uint256 yes = conditionalTokens.getPositionId(
+            usdc, conditionalTokens.getCollectionId(parentCollectionId, conditionId, 1)
+        );
+        uint256 no = conditionalTokens.getPositionId(
+            usdc, conditionalTokens.getCollectionId(parentCollectionId, conditionId, 2)
+        );
+        uint256 yesNo = conditionalTokens.getPositionId(
+            usdc, conditionalTokens.getCollectionId(parentCollectionId, conditionId, 3)
+        );
+
+        assertEq(conditionalTokens.balanceOf(address(0x42), yes), 1_000 * usdc.tokens());
+        assertEq(conditionalTokens.balanceOf(address(0x42), no), 1_000 * usdc.tokens());
+        assertEq(conditionalTokens.balanceOf(address(0x42), yesNo), 0);
+
+        conditionalTokens.safeTransferFrom(address(0x42), address(0x137), yes, 500 * usdc.tokens(), "");
+
+        assertEq(conditionalTokens.balanceOf(address(0x42), yes), 500 * usdc.tokens());
+        assertEq(conditionalTokens.balanceOf(address(0x137), yes), 500 * usdc.tokens());
+        assertEq(conditionalTokens.balanceOf(address(0x42), no), 1_000 * usdc.tokens());
+
+        uint256[] memory payouts = new uint256[](2);
+        payouts[0] = 1;
+        payouts[1] = 0;
+
+        vm.stopPrank();
+        vm.prank(oracle);
+
+        vm.expectEmit();
+        emit IConditionalTokens.ConditionResolution(conditionId, oracle, questionId, outcomeSlotCount, payouts);
+
+        conditionalTokens.reportPayouts(questionId, payouts);
+
+        vm.startPrank(address(0x42));
+
+        uint256[] memory indexSets = new uint256[](1);
+
+        indexSets[0] = 2; // No
+
+        vm.expectEmit();
+        emit IConditionalTokens.PayoutRedemption(address(0x42), usdc, parentCollectionId, conditionId, indexSets, 0);
+
+        conditionalTokens.redeemPositions(usdc, parentCollectionId, conditionId, indexSets);
+
+        assertEq(conditionalTokens.balanceOf(address(0x42), yes), 500 * usdc.tokens());
+        assertEq(conditionalTokens.balanceOf(address(0x42), no), 0);
+        assertEq(usdc.balanceOf(address(0x42)), usdc.initSupply() - 1_000 * usdc.tokens());
+
+        indexSets[0] = 1; // Yes
+
+        vm.expectEmit();
+        emit IConditionalTokens.PayoutRedemption(
+            address(0x42), usdc, parentCollectionId, conditionId, indexSets, 500 * usdc.tokens()
+        );
+
+        conditionalTokens.redeemPositions(usdc, parentCollectionId, conditionId, indexSets);
+
+        assertEq(conditionalTokens.balanceOf(address(0x42), yes), 0);
+        assertEq(conditionalTokens.balanceOf(address(0x42), no), 0);
+        assertEq(usdc.balanceOf(address(0x42)), usdc.initSupply() - 500 * usdc.tokens());
+
+        vm.stopPrank();
+        vm.startPrank(address(0x137));
+
+        assertEq(usdc.balanceOf(address(0x137)), 0);
+
+        indexSets[0] = 1; // Yes
+        conditionalTokens.redeemPositions(usdc, parentCollectionId, conditionId, indexSets);
+        assertEq(usdc.balanceOf(address(0x137)), 500 * usdc.tokens());
+    }
+}
